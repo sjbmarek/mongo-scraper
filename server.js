c4905c9504ca2a8289c9013d44a4633a504beef2
Susan Marek <smarek@chaskamn.com>
2018-04-01 19:33:32 -0500

    get all articles in db is working

--- server.js
+++ server.js
@@ -29,9 +29,11 @@ app.use(express.static("public"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/mongoscraper", {
-  useMongoClient: true
-});
+// mongoose.connect("mongodb://localhost/mongoscraper", {
+//   useMongoClient: true
+// });
+
+mongoose.connect("mongodb://localhost/mongoscraper");
 
 
 // Routes
@@ -42,67 +44,49 @@ app.get("/scrape", function(req, res) {
   request("https://www.teamusa.org/USA-Curling", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
-    var results = [];
 
     // Now, we grab every h3 within an article tag, and do the following:
     $("h3").each(function(i, element) {
-    //   // Save an empty result object
+      // Save an empty result object
       var result = {};
 
-    //   // Add the text and href of every link, and save them as properties of the result object
+      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
-        .text();
+      .children("a")
+      .text();
       result.link = $(this)
-        .children("a")
-        .attr("href");
+      .children("a")
+      .attr("href");
 
-    //   // Create a new Article using the `result` object built from scraping
+      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
-    //       // View the added result in the console
+      .then(function(dbArticle) {
+          // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
-    //       // If an error occurred, send it to the client
-          return res.json(err);
+      .catch(function(err) {
+          // If an error occurred, send it to the client
+          // return res.json(err);
+          return console.log(err);
         });
-    // });
-
-    // // If we were able to successfully scrape and save an Article, send a message to the client
-    // res.send("Scrape Complete");
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
     });
+    res.send("Scrape Complete");
   });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
 });
 
-// // Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article.find({})
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 // // Route for grabbing a specific Article by id, populate it with it's note
 // app.get("/articles/:id", function(req, res) {
