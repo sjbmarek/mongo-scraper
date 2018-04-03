87b28dc36f21a5a2d6aabf09df9b1c89525f1de4
Susan Marek <smarek@chaskamn.com>
2018-04-02 22:23:08 -0500

    initial styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,13 +6,13 @@ $(function() {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<h6 data-id='" + data[i]._id + "'>" + data[i].title + "</h6><p>" + data[i].link + "</p><hr>");
   }
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "h6", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
